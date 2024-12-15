6819fb8f7fec36882e9355d22041d43476f2f4ac
Vladyslav-Starostyn <starostin.vlad.od@gmail.com>
2024-12-16 01:36:21 +0200

    ----

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,50 +1,71 @@
-import { Container } from './App.styled';
-import Statistics from './Statistics/Statistics';
-import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
-import Notification from './Notification/Notification';
-import Section from './Section/Section';
-import { useState } from 'react';
-const App = () => {
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
+import { ContactList } from 'components/Contacts/ContactList';
+import { nanoid } from 'nanoid';
+import { Filter } from 'components/Filter/Filter';
+import { ContactForm } from 'components/ContactForm/ContactForm';
+import { Container } from 'components/App.styled';
+import { useEffect, useState } from 'react';
 
-  const countTotalFeedback = () => good + neutral + bad;
+export const App = () => {
+  const [contacts, setContacts] = useState(
+    () =>
+      JSON.parse(window.localStorage.getItem('contacts')) ?? [
+        { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+        { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+        { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+        { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      ]
+  );
+  const [filter, setfilter] = useState('');
 
-  const countPositiveFeedbackPercentage = () => {
-    return Math.round((good / countTotalFeedback()) * 100);
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+  const handleSubmit = (values, { resetForm }) => {
+    const { name, number } = values;
+    const contact = {
+      name,
+      number,
+    };
+
+    const dublicateContact = findDublicateContact(contact, contacts);
+    dublicateContact
+      ? alert(`${contact.name} is already in contacts`)
+      : setContacts([...contacts, { ...values, id: nanoid() }]);
+
+    resetForm();
   };
 
-  const stateUpdate = option => {
-    if (option === 'good') {
-      setGood(good + 1);
-    } else if (option === 'neutral') {
-      setNeutral(neutral + 1);
-    } else {
-      setBad(bad + 1);
-    }
+  const findDublicateContact = (contact, contactsList) => {
+    return contactsList.find(
+      item => item.name.toLowerCase() === contact.name.toLowerCase()
+    );
   };
 
-  const options = ['good', 'neutral', 'bad'];
+  const onFilterChange = e => setfilter(e.currentTarget.value);
+
+  const getFilteredContacts = () => {
+    const normalizedFilter = filter.toLowerCase();
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  };
+
+  const deleteContact = contactId =>
+    setContacts(contacts.filter(contact => contact.id !== contactId));
 
   return (
     <Container>
-      <Section title="Please leave feedback" />
-      <FeedbackOptions options={options} onBtnClick={stateUpdate} />
-      <Section title="Statistics" />
-      {countTotalFeedback() > 0 ? (
-        <Statistics
-          good={good}
-          neutral={neutral}
-          bad={bad}
-          total={countTotalFeedback}
-          positivePercentage={countPositiveFeedbackPercentage}
-        />
-      ) : (
-        <Notification message="No feedback given" />
+      <h1>Phonebook</h1>
+      <ContactForm onSubmit={handleSubmit} />
+      {contacts.length > 0 && <h2>Contacts</h2>}
+      {contacts.length > 0 && (
+        <Filter value={filter} onFilterChange={onFilterChange} />
       )}
+      <ContactList
+        contacts={getFilteredContacts()}
+        deleteContact={deleteContact}
+      />
     </Container>
   );
 };
-
-export default App;
--- src/components/App.styled.jsx
+++ src/components/App.styled.jsx
@@ -1,10 +1,8 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
-  margin: 50px auto;
-  width: 400px;
-  border: 3px solid #36426d;
-  border-radius: 8px;
-  box-shadow: 12px 12px 16px rgb(65, 64, 64), -12px -12px 16px rgb(91, 92, 94);
-  background-color: #c2c4ca;
+export const Container = styled.main`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
 `;
